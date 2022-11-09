1ee34980172ca3cefb2f711eaa228d5a66b91693
Haripriya Subramanian <1572382@TCS.com>
2022-11-09 10:15:50 +0100

    first python program

---
+++ firstpgm.py
@@ -0,0 +1,8 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = int(num1) + int(num2)
+
+# Display the sum
+print(f'The sum of {num1} and {num2} is {sum}')
