74a8b2744dc80966f4dd5051537a049c5bbac0c4
haripriyatud <haripriyasubramanian@gmail.com>
2022-11-10 14:45:53 +0100

    created a new folder for collections

--- conditions.py
+++ conditions.py
@@ -2,7 +2,7 @@ from datetime import date
 birth_year = input('Enter your year of birth: ')
 today = date.today()
 age= today.year - int(birth_year)
-if age>=18:
+if age >= 18:
     print("Congrats. You are allowed to vote");
 else:
     print("Sorry. You are too young to vote");
